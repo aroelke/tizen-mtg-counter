533b3f2af83511ff5b2b221aa8999d660f1cf634
Alec Roelke <alec.roelke@gmail.com>
2020-10-19 13:57:09 -0400

    Add simple bezel counter

--- TizenMtgCounter/App.cs
+++ TizenMtgCounter/App.cs
@@ -13,19 +13,7 @@ namespace TizenMtgCounter
 		public App()
 		{
 			// The root page of your application
-			MainPage = new ContentPage
-			{
-				Content = new StackLayout
-				{
-					VerticalOptions = LayoutOptions.Center,
-					Children = {
-						new Label {
-							HorizontalTextAlignment = TextAlignment.Center,
-							Text = "Welcome to Xamarin Forms!"
-						}
-					}
-				}
-			};
+			MainPage = new MainPage();
 		}
 
 		protected override void OnStart()
---
+++ TizenMtgCounter/MainPage.cs
@@ -0,0 +1,40 @@
+﻿using Tizen.Wearable.CircularUI.Forms;
+using Xamarin.Forms;
+
+namespace TizenMtgCounter
+{
+	public class MainPage : BezelInteractionPage, IRotaryEventReceiver
+	{
+		private int x;
+		private Label label;
+
+		public MainPage()
+		{
+			label = new Label
+			{
+				Text = "0",
+				FontSize = 32
+			};
+
+			Content = new StackLayout
+			{
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.Center,
+				Children = {
+					label
+				}
+			};
+
+			RotaryFocusObject = this;
+		}
+
+		public void Rotate(RotaryEventArgs args)
+		{
+			if (args.IsClockwise)
+				x++;
+			else
+				x--;
+			label.Text = x.ToString();
+		}
+	}
+}
