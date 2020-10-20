512ecd73450610304d9e50f6cecc3385c44dd372
Alec Roelke <alec.roelke@gmail.com>
2020-10-19 22:11:07 -0400

    Increase increment after continuous bezel use
    After leaving the bezel alone for 1/2 second, increment goes back to 1.

--- TizenMtgCounter/MainPage.cs
+++ TizenMtgCounter/MainPage.cs
@@ -1,20 +1,36 @@
-﻿using Tizen.Wearable.CircularUI.Forms;
+﻿using System.Timers;
+using Tizen.Wearable.CircularUI.Forms;
 using Xamarin.Forms;
 
 namespace TizenMtgCounter
 {
 	public class MainPage : BezelInteractionPage, IRotaryEventReceiver
 	{
-		private int x;
+		public const int TICK_THRESHOLD = 5;
+
+		private int life;
+		private int ticks;
 		private Label label;
+		private Timer resetThreshold;
 
 		public MainPage()
 		{
+			life = 0;
+			ticks = 0;
+
 			label = new Label
 			{
 				Text = "0",
 				FontSize = 32
 			};
+
+			resetThreshold = new Timer
+			{
+				Interval = 500,
+				Enabled = false,
+				AutoReset = false,
+			};
+			resetThreshold.Elapsed += (sender, e) => ticks = 0;
 
 			Content = new StackLayout
 			{
@@ -31,10 +47,31 @@ namespace TizenMtgCounter
 		public void Rotate(RotaryEventArgs args)
 		{
 			if (args.IsClockwise)
-				x++;
+			{
+				if (ticks >= 0)
+					ticks++;
+				else
+					ticks = 1;
+				if (ticks <= TICK_THRESHOLD)
+					life++;
+				else
+					life += TICK_THRESHOLD;
+			}
 			else
-				x--;
-			label.Text = x.ToString();
+			{
+				if (ticks <= 0)
+					ticks--;
+				else
+					ticks = -1;
+				if (ticks >= -TICK_THRESHOLD)
+					life--;
+				else
+					life -= TICK_THRESHOLD;
+			}
+			label.Text = life.ToString();
+
+			resetThreshold.Stop();
+			resetThreshold.Start();
 		}
 	}
 }
