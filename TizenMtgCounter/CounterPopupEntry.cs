64dbb98074b1c67f85ff39c7c7728ffcfdbf2c42
Alec Roelke <alec.roelke@gmail.com>
2021-02-01 14:58:40 -0500

    Add comments to CounterPopupEntry

--- TizenMtgCounter/CounterPopupEntry.cs
+++ TizenMtgCounter/CounterPopupEntry.cs
@@ -6,19 +6,40 @@ using Xamarin.Forms.Platform.Tizen;
 [assembly: ExportRenderer(typeof(TizenMtgCounter.CounterPopupEntry), typeof(TizenMtgCounter.CounterPopupEntryRenderer))]
 namespace TizenMtgCounter
 {
-	public class CounterPopupEntry : PopupEntry {}
+	/// <summary>
+	/// Special <see cref="PopupEntry"/> for rendering with large text. When tapped,
+	/// the font size reduces to fit the window with the keyboard.
+	/// </summary>
+	public class CounterPopupEntry : PopupEntry
+	{
+		/// <summary>
+		/// Font size of the entry text while the keyboard is active.
+		/// </summary>
+		public const int FocusFontSize = 16;
 
+		public CounterPopupEntry() : base() {}
+	}
+
+	/// <summary>
+	/// Renderer for <see cref="CounterPopupEntry"/> that handles the text size changing while
+	/// the keyboard is up.
+	/// </summary>
 	internal class CounterPopupEntryRenderer : PopupEntryRenderer
 	{
-		public CounterPopupEntryRenderer() : base() { }
+		public CounterPopupEntryRenderer() : base() {}
 
+		/// <summary>
+		/// When the <see cref="CounterPopupEntry"/> is tapped and the keyboard is brought up,
+		/// change the font size so it fits. Change it back if the keyboard is dismissed.
+		/// </summary>
+		/// <param name="e"></param>
 		protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
 		{
 			base.OnElementChanged(e);
 			if (Control != null)
 			{
 				double size = e.NewElement.FontSize;
-				Control.Clicked += (sender, v) => e.NewElement.FontSize = 16;
+				Control.Clicked += (sender, v) => e.NewElement.FontSize = CounterPopupEntry.FocusFontSize;
 				e.NewElement.Completed += (sender, v) => e.NewElement.FontSize = size;
 			}
 		}
