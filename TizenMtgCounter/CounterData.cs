8380c0684bbe476e75e5137f1d71e3bee0d9058f
Alec Roelke <alec.roelke@gmail.com>
2021-02-01 16:52:20 -0500

    Add comments to CounterData

--- TizenMtgCounter/CounterData.cs
+++ TizenMtgCounter/CounterData.cs
@@ -6,20 +6,21 @@ using Xamarin.Forms;
 
 namespace TizenMtgCounter
 {
+	/// <summary>
+	/// Backing data containing counters for tracking one part of a game state.
+	/// </summary>
 	public class CounterData : INotifyPropertyChanged
 	{
 		private int val = 0;
 
+		/// <summary>
+		/// Create a new backing data set.
+		/// </summary>
 		public CounterData() {}
 
-		public CounterData(CounterData original)
-		{
-			val = original.val;
-			Minimum = original.Minimum;
-			Maximum = original.Maximum;
-			Thresholds = original.Thresholds;
-		}
-
+		/// <summary>
+		/// Get or set the value of the quantity being tracked.
+		/// </summary>
 		public int Value {
 			get => val;
 			set
@@ -30,9 +31,20 @@ namespace TizenMtgCounter
 			}
 		}
 
+		/// <summary>
+		/// Get or set the minimum value of the quantity being tracked.
+		/// </summary>
 		public int Minimum { get; set; } = int.MinValue;
+
+		/// <summary>
+		/// Get or set the maximum value of the quantity being tracked.
+		/// </summary>
 		public int Maximum { get; set; } = int.MaxValue;
 
+		/// <summary>
+		/// Get or set the thresholds that control the color of the counter displaying the quantity
+		/// being tracked.
+		/// </summary>
 		public IList<(int, Color)> Thresholds { get; set; } = new List<(int, Color)>();
 		public Color TextColor
 		{
@@ -45,11 +57,18 @@ namespace TizenMtgCounter
 			}
 		}
 
+		/// <summary>
+		/// Fire all of the property change listeners.
+		/// </summary>
+		/// <param name="name">Name of the property being changed.</param>
 		private void OnPropertyChanged([CallerMemberName] string name = null)
 		{
 			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
 		}
 
+		/// <summary>
+		/// Occurs whenever the value of the quantity being tracked changes.
+		/// </summary>
 		public event PropertyChangedEventHandler PropertyChanged;
 	}
 }
