810d1e9804b8e6eeae850c6c37fe76d978524a7d
Alec Roelke <alec.roelke@gmail.com>
2021-02-01 14:06:39 -0500

    Add comments to RepeatButton

--- TizenMtgCounter/RepeatButton.cs
+++ TizenMtgCounter/RepeatButton.cs
@@ -4,8 +4,14 @@ using Xamarin.Forms;
 
 namespace TizenMtgCounter
 {
+	/// <summary>
+	/// <see cref="Button"/> that repeats its action while held down.
+	/// </summary>
 	public class RepeatButton : Button
 	{
+		/// <summary>
+		/// Create a new <c>RepeatButton</c> with default repeat delay and time.
+		/// </summary>
 		public RepeatButton() : base()
 		{
 			Timer timer = new Timer
@@ -25,10 +31,15 @@ namespace TizenMtgCounter
 			Released += (sender, e) => timer.Stop();
 		}
 
+		/// <value>Gets or sets delay in ms after the button is pressed before repetition starts.</value>
 		public double Delay { get; set; } = 100;
 
+		/// <value>Gets or sets delay in ms between repetitions while the button is held.</value>
 		public double Interval { get; set; } = 100;
 
+		/// <summary>
+		/// Occurs every <see cref="Delay"/> ms while the button is held after <see cref="Interval"/> ms of initial press.
+		/// </summary>
 		public event EventHandler Held;
 	}
 }
