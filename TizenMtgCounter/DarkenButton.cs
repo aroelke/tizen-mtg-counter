1313f10590f5773a7fcdcc5077790bc75fc96766
Alec Roelke <alec.roelke@gmail.com>
2021-02-01 14:54:14 -0500

    Add comments to DarkenButton

--- TizenMtgCounter/DarkenButton.cs
+++ TizenMtgCounter/DarkenButton.cs
@@ -2,8 +2,15 @@
 
 namespace TizenMtgCounter
 {
+	/// <summary>
+	/// <see cref="ImageButton"/> that becomes transleucent while held down.
+	/// Against a black background, this creates a darkening effect.
+	/// </summary>
 	class DarkenButton : ImageButton
 	{
+		/// <summary>
+		/// Create a new <c>DarkenButton</c>.
+		/// </summary>
 		public DarkenButton() : base()
 		{
 			Opacity = ReleasedOpacity;
@@ -11,7 +18,14 @@ namespace TizenMtgCounter
 			Released += (sender, e) => Opacity = ReleasedOpacity;
 		}
 
+		/// <summary>
+		/// Get or set the opacity of the button while it's held down.
+		/// </summary>
 		public double HeldOpacity { get; set; } = 1.0/3.0;
+
+		/// <summary>
+		/// Get or set the opacity while the button is not held down.
+		/// </summary>
 		public double ReleasedOpacity { get; set; } = 1;
 	}
 }
